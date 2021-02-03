3b33b84c965c63d58cd6bdca6b892b0a53354ad5
spencercreer <spencercreer@gmail.com>
2021-02-02 19:11:48 -0700

    displayed result text of coin flip to screen

--- develop/script/coin.js
+++ develop/script/coin.js
@@ -3,19 +3,20 @@ jQuery(document).ready(function($){
     $('.coin-buttons').on('click', function(){
       $(".coin-buttons").prop("disabled", true);
       var flipResult = Math.ceil(Math.random()*2);
-      console.log(flipResult)
+      var flipResultText;
       $('#coin').removeClass();
       setTimeout(function(){
         $(".coin-buttons").prop("disabled", false);
+        $(".flip-result").text(flipResultText);
       },3000)
       setTimeout(function(){
         if(flipResult === 1){
           $('#coin').addClass('heads');
-          $(".flip-result").text('it is heads');
+          flipResultText = "Heads";
         }
         else{
           $('#coin').addClass('tails');
-          $(".flip-result").text('it is tails');
+          flipResultText = "Tails";
         }
       }, 100);
       
