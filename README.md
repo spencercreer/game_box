dc0bf505106d31f626f921aaab4d77313c75eff7
spencercreer <spencercreer@gmail.com>
2021-02-13 22:45:38 -0700

    added cards

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # math_random
-Random generator
+Math.random uses JavaScript math functions to simulate rolling dice, flipping a coin, or picking a card.
+
+## Dice Roll
+The dice roll application begins with one die shown and five dice hidden. When the user clicks the "Add" button, the next die is shown. When the "Roll" button is clicked the dice display a random number fifteen times. The final dice values are displayed to the screen along with their sum a product.
+
+## Coin Flip
+
+## Pick a Card
+
+### Skills
+1. Math.random, Math.floor, Math.ciel
+2. jQuery show and hide functions
+3. CSS relative and absolute positioning
+4. CSS animations and @keyframes
+
+
--- develop/script/card.js
+++ develop/script/card.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var playingCards = ["joker", "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+    var playingCards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
     var suites = ["hearts", "diamonds", "clubs", "spades"];
 
     $(".pickBtn").on("click",function(){
@@ -10,6 +10,9 @@ $(document).ready(function() {
         var cardNum = Math.floor(Math.random()*14);
         var card = playingCards[cardNum];
         console.log(card);
-        $(".cardFront").css("background-image", "url('../assets/" + card + "_" + cardSuite + ".JPG'")
+        // I believe this will need to change to cardFront to get a Flip
+        $(".cardBack").css("background-image", "url('../assets/" + card + "_" + cardSuite + ".JPG'")
+        // Add class for card flip based on flipResult
+        $("#playingCard").addClass(".cardFront");
     });
 });
