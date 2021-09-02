87fecdadc3d01f9e8874442887dad07c9e62f82f
spencercreer <spencercreer@gmail.com>
2021-09-01 22:02:38 -0700

    update card image

--- develop/script/card.js
+++ develop/script/card.js
@@ -1,34 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var deckCount
     var deck
     var cardsRemaining
 
-    function callCardsApi() {
-        let queryURL = 'http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-        .then((res) => {
-            deck = res.deck_id
-            cardsRemaining = res.remaining
-            console.log(deck)
-        })
-        .fail(() => console.log('Error: 404 not found'))
-    }
-    callCardsApi()
-
-    $(".pickBtn").on("click",function(){
+    $("#pickBtn").on("click", function () {
         console.log(deck)
         let queryURL = `http://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
         $.ajax({
             url: queryURL,
             method: 'GET'
         })
-        .then((res) => {
-            console.log(res)
-            $(".card").css("background-image", `url(${res.cards[0].image})`)
-        })
-        .fail(() => console.log('Error: 404 not found'))
+            .then((res) => {
+                console.log(res)
+                $(".card").css("background-image", `url(${res.cards[0].image})`)
+            })
+            .fail(() => console.log('Error: 404 not found'))
     });
 
+    $("#shuffleBtn").on("click", function () {
+        console.log("Shuffle")
+    });
+
+    function getCardDeck() {
+        let queryURL = 'http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+            .then((res) => {
+                deck = res.deck_id
+                cardsRemaining = res.remaining
+                console.log(deck)
+            })
+            .fail(() => console.log('Error: 404 not found'))
+    }
+    getCardDeck()
 });
