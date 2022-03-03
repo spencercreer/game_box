a411d1a10568c1098a4e4e9988875cbb60fd5d65
spencercreer <spencercreer@gmail.com>
2022-03-02 23:23:10 -0700

    logo

--- game-box/src/components/Header.js
+++ game-box/src/components/Header.js
@@ -1,18 +1,27 @@
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Image } from 'react-bootstrap'
 
+import logo from '../assets/logo310.png'
+
+const { Brand, Toggle, Collapse } = Navbar
 const { Link } = Nav
 
 const Header = ({ setCurrentPage }) => {
 
     return (
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
+            <div style={{ marginLeft: '15px' }}>
+                <Brand href="#home" style={{ fontSize: '35px', height: '20px' }}>
+                    <Image src={logo} style={{ borderRadius: '2px', width: '35px', marginRight: '10px' }} />
+                    Game Box
+                </Brand>
+            </div>
+            <Toggle aria-controls="responsive-navbar-nav" />
+            <Collapse id="responsive-navbar-nav" className="mx-3">
                 <Nav className="me-auto">
                     <Link onClick={() => setCurrentPage('dice')}>Dice</Link>
                     <Link onClick={() => setCurrentPage('scorecard')}>Scorecard</Link>
                 </Nav>
-            </Container>
+            </Collapse>
         </Navbar>
     )
 }
