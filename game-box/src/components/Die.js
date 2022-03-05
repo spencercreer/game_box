7d04a43e98518e36f135a7ab6849800cc3690930
spencercreer <spencercreer@gmail.com>
2022-03-04 21:24:39 -0700

    dice number style

--- game-box/src/components/Die.js
+++ game-box/src/components/Die.js
@@ -3,21 +3,41 @@ import "./dice.css"
 const Die = ({ roll, number, color }) => {
     console.log(roll, number, color)
 
-    // let diceStyle
-    // diceStyle = number === 2 ? "dice2" : "dice6"
+    let show
+
+    switch (number) {
+        case 1:
+            show = [true, false, false, false, false, false, false]
+            break;
+        case 2:
+            show = [false, true, true, false, false, false, false]
+            break;
+        case 3:
+            show = [true, true, true, false, false, false, false]
+            break;
+        case 4:
+            show = [false, true, true, true, true, false, false]
+            break;
+        case 5:
+            show = [true, true, true, true, true, false, false]
+            break;
+        default:
+            show = [false, true, true, true, true, true, true]
+            break;
+    }
 
     // TODO: cleanup
-    const dotColor = color !== 'white' ? 'whiteDot' : ''
+    const dotStyle = color !== 'white' ? 'dot whiteDot' : 'dot'
 
     return (
         <div className={`dice ${color}`}>
-            <div className={`dot ${dotColor} dot1`}></div>
-            <div className={`dot ${dotColor} dot2`}></div>
-            <div className={`dot ${dotColor} dot3`}></div>
-            <div className={`dot ${dotColor} dot4`}></div>
-            <div className={`dot ${dotColor} dot5`}></div>
-            <div className={`dot ${dotColor} dot6`}></div>
-            <div className={`dot ${dotColor} dot7`}></div>
+            <div style={{ display: show[0] ? 'block' : 'none' }} className={`${dotStyle} dot1`}></div>
+            <div style={{ display: show[1] ? 'block' : 'none' }} className={`${dotStyle} dot2`}></div>
+            <div style={{ display: show[2] ? 'block' : 'none' }} className={`${dotStyle} dot3`}></div>
+            <div style={{ display: show[3] ? 'block' : 'none' }} className={`${dotStyle} dot4`}></div>
+            <div style={{ display: show[4] ? 'block' : 'none' }} className={`${dotStyle} dot5`}></div>
+            <div style={{ display: show[5] ? 'block' : 'none' }} className={`${dotStyle} dot6`}></div>
+            <div style={{ display: show[6] ? 'block' : 'none' }} className={`${dotStyle} dot7`}></div>
         </div>
     )
 }
